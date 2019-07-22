35836af8abd26ae998f630b455d774201a022da5
ecovington35 <jurassicethan@gmail.com>
2019-07-22 15:32:56 -0600

    Blog

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
-  }
+  },
+{
+  path: 'blog',
+  component: BlogComponent
+}
+
 ];
 
 @NgModule({
